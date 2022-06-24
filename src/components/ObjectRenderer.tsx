d4239230ffa50d65a8c0a1720b216f9d2eb655f5
Chrystal Lyu <chrystal.lyu@gmail.com>
2022-06-24 10:31:34 -0700

    cleanup template

---
+++ src/components/ObjectRenderer.tsx
@@ -0,0 +1,13 @@
+export interface ObjectRendererProps {
+  input: object;
+}
+
+const ObjectRenderer = () => {
+  return (
+    <div>
+      <div>render</div>
+    </div>
+  );
+};
+
+export default ObjectRenderer;
